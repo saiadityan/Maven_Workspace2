32f6d24458215cc7547a51bed56d20df17a8245d
saiadityan <saiadityan18@gmail.com>
2023-01-09 14:22:47 +0530

    my new update

---
+++ src/test/java/pkg3/ClassC.java
@@ -0,0 +1,13 @@
+package pkg3;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassC 
+{
+  @Test
+  public void finatest() 
+  {
+	  Reporter.log("finaltest executed");
+  }
+}
