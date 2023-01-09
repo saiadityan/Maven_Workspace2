1f86e550219a36d0b5a1857a416ceaec3287060b
saiadityan <saiadityan18@gmail.com>
2023-01-09 14:18:28 +0530

    my second update

---
+++ src/test/java/pkg1/ClassB.java
@@ -0,0 +1,30 @@
+package pkg1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassB 
+{
+	@Test
+	  public void tc004() 
+	  {
+		  Reporter.log("tc004 executed successfull", true);
+	  }
+	  @Test
+	  public void tc005() 
+	  {
+		  Reporter.log("tc005 executed successfull", true);
+	  }
+	  
+	  @Test
+	  public void tc006() 
+	  {
+		  Reporter.log("tc006 executed successfull", true);
+	  }
+	 @Test
+	  public void Logout() 
+	  {
+		  Reporter.log("Logout test successfull", true);
+	  }
+
+}
---
+++ src/test/java/pkg2/ClassA.java
@@ -0,0 +1,29 @@
+package pkg2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassA 
+{
+  @Test
+  public void tc001() 
+  {
+	  Reporter.log("tc001 executed successfull", true);
+  }
+  @Test
+  public void tc002() 
+  {
+	  Reporter.log("tc002 executed successfull", true);
+  }
+  
+  @Test
+  public void tc003() 
+  {
+	  Reporter.log("tc003 executed successfull", true);
+  }
+  @Test
+  public void Login() 
+  {
+	  Reporter.log("Login test successfull", true);
+  }
+}
