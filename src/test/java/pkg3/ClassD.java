4e46ed1d2617b3dbd5cb2ffa0485f1998ba74a83
saiadityan <saiadityan18@gmail.com>
2023-01-09 14:35:07 +0530

    my second update

---
+++ src/test/java/pkg3/ClassD.java
@@ -0,0 +1,19 @@
+package pkg3;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassD 
+{
+	 @Test
+	  public void tc006() 
+	  {
+		  Reporter.log("tc006 executed successfull", true);
+	  }
+	 @Test
+	  public void Logout() 
+	  {
+		  Reporter.log("Logout test successfull", true);
+	  }
+
+}
